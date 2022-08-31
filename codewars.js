58b4779d978b4cd6ec98f046c7fb485bf0a9ea2f
Artem <zhur2305@gmail.com>
2022-08-31 20:23:16 +0300

    added task

--- codewars.js
+++ codewars.js
@@ -71,6 +71,12 @@ Should return: 160 (the only even number)
 
 
 
+// Complete the solution so that it reverses all of the words within the string passed in.
+function reverseWords(str){
+  return str.split(' ').reverse().join(' '); // reverse those words
+}
+
+
 
 
 
