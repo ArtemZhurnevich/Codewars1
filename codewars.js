f8d0745a81bdd4d493ec7def094d72370739bcf0
Artem <zhur2305@gmail.com>
2022-09-01 07:45:51 +0300

    added task

--- codewars.js
+++ codewars.js
@@ -108,6 +108,11 @@ function oddCount(n) {
   }
 
 
+  // Gravity Flip
+  const flip=(d, a)=>{
+  
+    return d === "R" ? a.sort(function(x, y){return x-y}) : a.sort(function(x, y){return y-x})
+  }
 
 
 
